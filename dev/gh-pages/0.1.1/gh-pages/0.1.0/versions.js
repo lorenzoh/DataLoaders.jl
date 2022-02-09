96f2328643a66d228bbc9b027ce4b802dfbaa817
lorenzoh <lorenz.ohly@gmail.com>
2022-02-09 19:27:46 +0100

    wipe

--- dev/gh-pages/0.1.1/gh-pages/0.1.0/versions.js
+++
@@ -1,6 +0,0 @@
-var PUBLISH_ROOT = '/DataLoaders.jl/0.1.0';
-var PUBLISH_VERSION = "0.1.0";
-var PUBLISH_VERSIONS = [
-    ["dev","/DataLoaders.jl/dev/index.html"],
-    ["0.1.0","/DataLoaders.jl/0.1.0/index.html"],
-];
--- dev/versions.js
+++
@@ -1,9 +0,0 @@
-var PUBLISH_ROOT = '/DataLoaders.jl/dev';
-var PUBLISH_VERSION = "dev";
-var PUBLISH_VERSIONS = [
-    ["test","/DataLoaders.jl/test/index.html"],
-    ["dev","/DataLoaders.jl/dev/index.html"],
-    ["0.1.2","/DataLoaders.jl/0.1.2/index.html"],
-    ["0.1.1","/DataLoaders.jl/0.1.1/index.html"],
-    ["0.1.0","/DataLoaders.jl/0.1.0/index.html"],
-];
